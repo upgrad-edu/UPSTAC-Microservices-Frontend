487c1ab50b3c92ec00a47bbabf1cecfbf5aa8772
vaishalinankani08 <77958988+vaishalinankani08@users.noreply.github.com>
2021-03-31 15:45:36 +0530

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -10,11 +10,19 @@ if (cloudHosts.includes(hostname)) {
   baseUrl = "http://localhost:8080";
 }
 
-const DOCUMENT_UPLOAD_URL = "http://localhost:8082/documents/upload/";
-const REGISTER_URL = "http://localhost:8090/auth/register";
-const DOCTOR_REGISTER_URL = "http://localhost:8090/auth/doctor/register";
-const TESTER_REGISTER_URL = "http://localhost:8090/auth/tester/register";
-const DOWNLOAD_BASE_URL = "http://localhost:8082/documents/download/";
+console.log('REACT_APP_DOCSVC_HOST' + process.env.REACT_APP_DOCSVC_HOST)
+console.log('REACT_APP_REGSVC_HOST' + process.env.REACT_APP_REGSVC_HOST)
+console.log('REACT_APP_MASTERSVC_HOST' + process.env.REACT_APP_MASTERSVC_HOST)
+const docSvc = process.env.REACT_APP_DOCSVC_HOST;
+const regSvc = process.env.REACT_APP_REGSVC_HOST;
+const masterSvc = process.env.REACT_APP_MASTERSVC_HOST;
+
+const DOCUMENT_UPLOAD_URL = "http://"+docSvc+":8082/documents/upload/";
+const REGISTER_URL = "http://"+regSvc+":8090/auth/register";
+const DOCTOR_REGISTER_URL = "http://"+regSvc+":8090/auth/doctor/register";
+const TESTER_REGISTER_URL = "http://"+regSvc+":8090/auth/tester/register";
+const DOWNLOAD_BASE_URL = "http://"+docSvc+":8082/documents/download/";
+
 
 export const environment = {
   baseUrl,
